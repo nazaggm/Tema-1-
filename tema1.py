4e8f04441f3c6c0ec786f243718fb83d5cad4273
Nazaret <0611518829@uma.es>
2024-09-11 19:12:55 +0200

    naza tema1.py

---
+++ tema1.py
@@ -0,0 +1,28 @@
+from turtle import * 
+left(180) 
+forward (10) 
+left (180) 
+forward (10) 
+#Hacer un cuadrado 
+forward(80)
+left (90)
+forward (80) 
+left (90) 
+forward(80)
+left(90)
+forward(80) 
+# Avanzamos 80 posiciones 
+lado:int=300 
+lado : int= (input: ("Dime el lado"))
+#Hacer un triángulo 
+num_lados: int = 3 
+angulo:float = 360/num_lados 
+forward(lado)
+left(120)
+forward(lado) 
+left(120)
+forward(lado) 
+#Hacer un pentágono 
+for i in range (num_lados)
+forward(lado) 
+left(angulo) 
